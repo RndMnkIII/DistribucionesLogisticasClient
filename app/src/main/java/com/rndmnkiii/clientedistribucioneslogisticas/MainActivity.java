c55783b6a178a57a2efb33540cb38575d9cb5711
FT81xMania <ft81xmania@gmail.com>
2018-03-26 19:39:15 +0200

    Commit inicial proyecto Android ClienteDistribucionesLogisticas

---
+++ app/src/main/java/com/rndmnkiii/clientedistribucioneslogisticas/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rndmnkiii.clientedistribucioneslogisticas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
